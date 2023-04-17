400414c755e015aa39eb051077001aa407289fff
ynsemreyildirim <yunusemreyildirim.inf.001@gmail.com>
2023-04-17 18:40:36 +0300

    added scaffolds navBar etc.

--- app/src/main/java/com/yunusemreyildirim/instaclone/component/StoryAndProfilePhoto.kt
+++ app/src/main/java/com/yunusemreyildirim/instaclone/component/StoryAndProfilePhoto.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -13,34 +14,38 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 
 @Composable
 fun StoryAndProfilePhoto(
 ) {
     val borderBrush = Brush.linearGradient(colors = listOf(Color.Red, Color.Green, Color.Blue))
-    Row {
-        Spacer(modifier = Modifier.width(6.dp))
-        Box(
-            modifier = Modifier
-                .size(76.dp)
-                .clip(CircleShape)
-                .border(width = 2.dp, brush = borderBrush, shape = CircleShape)
-                .clickable(onClick = {
-                    //go profile
-                }),
-            contentAlignment = Alignment.Center
-        ) {
-            AsyncImage(
-                model = photo,
-                contentDescription = "post profile photo",
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Row {
+            Spacer(modifier = Modifier.width(6.dp))
+            Box(
                 modifier = Modifier
+                    .size(76.dp)
                     .clip(CircleShape)
-                    .size(65.dp),
-                contentScale = ContentScale.Crop
-            )
+                    .border(width = 2.dp, brush = borderBrush, shape = CircleShape)
+                    .clickable(onClick = {
+                        //go profile
+                    }),
+                contentAlignment = Alignment.Center
+            ) {
+                AsyncImage(
+                    model = photo,
+                    contentDescription = "post profile photo",
+                    modifier = Modifier
+                        .clip(CircleShape)
+                        .size(65.dp),
+                    contentScale = ContentScale.Crop
+                )
+            }
+            Spacer(modifier = Modifier.width(6.dp))
         }
-        Spacer(modifier = Modifier.width(6.dp))
+        Text(text = "username_x", fontSize = 13.sp, modifier = Modifier.padding(top = 4.dp))
     }
 }
 
--- app/src/main/java/com/yunusemreyildirim/instaclone/view/MainActivity.kt
+++ app/src/main/java/com/yunusemreyildirim/instaclone/view/MainActivity.kt
@@ -1,15 +1,26 @@
 package com.yunusemreyildirim.instaclone.view
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.yunusemreyildirim.instaclone.R
 import com.yunusemreyildirim.instaclone.ui.theme.InstaCloneTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -18,9 +29,58 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    FeedPage(this@MainActivity)
+                    MainPagesWithScaffold()
                 }
             }
         }
+    }
+
+    @Composable
+    fun Pages() {
+        val navController = rememberNavController()
+        NavHost(navController = navController, startDestination = "FeedPage") {
+            composable("FeedPage") {
+                FeedPage(context = this@MainActivity)
+            }
+        }
+    }
+
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+    @Composable
+    fun MainPagesWithScaffold() {
+        Scaffold(bottomBar = {
+            BottomAppBar(
+                modifier = Modifier
+                    .padding(all = 10.dp)
+                    .clip(CircleShape)
+            ) {
+                BottomNavigationItem(selected = true, onClick = {
+
+                }, icon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.home),
+                        contentDescription = "home icon"
+                    )
+                })
+                BottomNavigationItem(selected = false, onClick = {
+
+                }, icon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.search),
+                        contentDescription = "home icon"
+                    )
+                })
+                BottomNavigationItem(selected = false, onClick = {
+
+                }, icon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.person),
+                        contentDescription = "home icon"
+                    )
+                })
+            }
+        }, content = {
+            Pages()
+        })
     }
 }
