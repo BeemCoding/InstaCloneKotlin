f75261d902e40f32e128f76f9b63e979b09ab86c
ynsemreyildirim <yunusemreyildirim.inf.001@gmail.com>
2023-04-17 15:18:13 +0300

    solved back press-modal bottom sheet

--- app/src/main/java/com/yunusemreyildirim/instaclone/view/FeedPage.kt
+++ app/src/main/java/com/yunusemreyildirim/instaclone/view/FeedPage.kt
@@ -1,5 +1,6 @@
 package com.yunusemreyildirim.instaclone.view
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
@@ -7,25 +8,31 @@ import androidx.compose.runtime.rememberCoroutineScope
 import com.yunusemreyildirim.instaclone.component.Comment
 import com.yunusemreyildirim.instaclone.component.PostImage
 import com.yunusemreyildirim.instaclone.component.SubComment
-import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalMaterialApi::class, DelicateCoroutinesApi::class)
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun FeedPage() {
+
     val scope = rememberCoroutineScope()
     val bottomSheetState = rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Hidden)
-    ModalBottomSheetLayout(sheetState = bottomSheetState,sheetContent = {
-        LazyColumn{
-            item { Comment() }
-            item { Comment() }
-            item { SubComment() }
-            item { SubComment() }
-            item { Comment() }
-            item { Comment() }
-            item { SubComment() }
-        }
-    },) {
+    ModalBottomSheetLayout(
+        sheetState = bottomSheetState,
+        sheetContent = {
+            BackHandler {
+                scope.launch { bottomSheetState.hide() }
+            }
+            LazyColumn {
+                item { Comment() }
+                item { Comment() }
+                item { SubComment() }
+                item { SubComment() }
+                item { Comment() }
+                item { Comment() }
+                item { SubComment() }
+            }
+        },
+    ) {
         LazyColumn {
             item { PostImage{
                 scope.launch { bottomSheetState.show() }
