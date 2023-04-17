816face7e049a0c626b78e627b4840478a9f22cf
ynsemreyildirim <yunusemreyildirim.inf.001@gmail.com>
2023-04-17 19:06:35 +0300

    solved navbar and comment bottom sheet's display problem

--- app/src/main/java/com/yunusemreyildirim/instaclone/view/FeedPage.kt
+++ app/src/main/java/com/yunusemreyildirim/instaclone/view/FeedPage.kt
@@ -1,87 +1,55 @@
 package com.yunusemreyildirim.instaclone.view
 
-import android.app.Activity
-import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.yunusemreyildirim.instaclone.component.Comment
 import com.yunusemreyildirim.instaclone.component.PostImage
 import com.yunusemreyildirim.instaclone.component.StoryAndProfilePhoto
-import com.yunusemreyildirim.instaclone.component.SubComment
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun FeedPage(context: Activity) {
-
-    val scope = rememberCoroutineScope()
-    val bottomSheetState =
-        rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Hidden)
-    ModalBottomSheetLayout(
-        sheetState = bottomSheetState,
-        sheetContent = {
-
-            BackHandler {
-                if (bottomSheetState.currentValue != ModalBottomSheetValue.Hidden) {
-                    scope.launch { bottomSheetState.hide() }
-                } else {
-                    context.finish()
-                }
-            }
-            LazyColumn {
-                item { Comment() }
-                item { Comment() }
-                item { SubComment() }
-                item { SubComment() }
-                item { Comment() }
-                item { Comment() }
-                item { SubComment() }
-            }
-        },
-    ) {
-        LazyColumn(modifier = Modifier.fillMaxSize()) {
-            item {
-                LazyRow(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .wrapContentHeight(),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    item { StoryAndProfilePhoto() }
-                    item { StoryAndProfilePhoto() }
-                    item { StoryAndProfilePhoto() }
-                    item { StoryAndProfilePhoto() }
-                    item { StoryAndProfilePhoto() }
-                    item { StoryAndProfilePhoto() }
-                }
-            }
-            item {
-                PostImage {
-                    scope.launch { bottomSheetState.show() }
-                }
-            }
-            item {
-                PostImage {
-                    scope.launch { bottomSheetState.show() }
-                }
-            }
-            item {
-                PostImage {
-                    scope.launch { bottomSheetState.show() }
-                }
-            }
-            item {
-                PostImage {
-                    scope.launch { bottomSheetState.show() }
-                }
+fun FeedPage(bottomSheetState: ModalBottomSheetState, scope:CoroutineScope) {
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        item {
+            LazyRow(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                item { StoryAndProfilePhoto() }
+                item { StoryAndProfilePhoto() }
+                item { StoryAndProfilePhoto() }
+                item { StoryAndProfilePhoto() }
+                item { StoryAndProfilePhoto() }
+                item { StoryAndProfilePhoto() }
             }
         }
-
+        item {
+            PostImage {
+                scope.launch { bottomSheetState.show() }
+            }
+        }
+        item {
+            PostImage {
+                scope.launch { bottomSheetState.show() }
+            }
+        }
+        item {
+            PostImage {
+                scope.launch { bottomSheetState.show() }
+            }
+        }
+        item {
+            PostImage {
+                scope.launch { bottomSheetState.show() }
+            }
+        }
     }
 }
--- app/src/main/java/com/yunusemreyildirim/instaclone/view/MainActivity.kt
+++ app/src/main/java/com/yunusemreyildirim/instaclone/view/MainActivity.kt
@@ -1,14 +1,20 @@
+@file:OptIn(ExperimentalMaterialApi::class)
+
 package com.yunusemreyildirim.instaclone.view
 
 import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -17,10 +23,15 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.yunusemreyildirim.instaclone.R
+import com.yunusemreyildirim.instaclone.component.Comment
+import com.yunusemreyildirim.instaclone.component.SubComment
 import com.yunusemreyildirim.instaclone.ui.theme.InstaCloneTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
-
+    private lateinit var bottomSheetState:ModalBottomSheetState
+    private lateinit var scope:CoroutineScope
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -29,6 +40,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    bottomSheetState = rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Hidden)
+                    scope = rememberCoroutineScope()
                     MainPagesWithScaffold()
                 }
             }
@@ -40,47 +53,71 @@ class MainActivity : ComponentActivity() {
         val navController = rememberNavController()
         NavHost(navController = navController, startDestination = "FeedPage") {
             composable("FeedPage") {
-                FeedPage(context = this@MainActivity)
+                FeedPage(bottomSheetState, scope)
             }
         }
     }
 
+    @OptIn(ExperimentalMaterialApi::class)
     @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
     @Composable
     fun MainPagesWithScaffold() {
-        Scaffold(bottomBar = {
-            BottomAppBar(
-                modifier = Modifier
-                    .padding(all = 10.dp)
-                    .clip(CircleShape)
-            ) {
-                BottomNavigationItem(selected = true, onClick = {
+        ModalBottomSheetLayout(
+            sheetState = bottomSheetState, sheetShape = RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp),
+            sheetContent = {
 
-                }, icon = {
-                    Icon(
-                        painter = painterResource(id = R.drawable.home),
-                        contentDescription = "home icon"
-                    )
-                })
-                BottomNavigationItem(selected = false, onClick = {
+                BackHandler {
+                    if (bottomSheetState.currentValue != ModalBottomSheetValue.Hidden) {
+                        scope.launch { bottomSheetState.hide() }
+                    } else {
+                        finish()
+                    }
+                }
+                LazyColumn {
+                    item { Comment() }
+                    item { Comment() }
+                    item { SubComment() }
+                    item { SubComment() }
+                    item { Comment() }
+                    item { Comment() }
+                    item { SubComment() }
+                }
+            },
+        ) {
+            Scaffold(bottomBar = {
+                BottomAppBar(
+                    modifier = Modifier
+                        .padding(all = 10.dp)
+                        .clip(CircleShape)
+                ) {
+                    BottomNavigationItem(selected = true, onClick = {
 
-                }, icon = {
-                    Icon(
-                        painter = painterResource(id = R.drawable.search),
-                        contentDescription = "home icon"
-                    )
-                })
-                BottomNavigationItem(selected = false, onClick = {
+                    }, icon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.home),
+                            contentDescription = "home icon"
+                        )
+                    })
+                    BottomNavigationItem(selected = false, onClick = {
 
-                }, icon = {
-                    Icon(
-                        painter = painterResource(id = R.drawable.person),
-                        contentDescription = "home icon"
-                    )
-                })
-            }
-        }, content = {
-            Pages()
-        })
+                    }, icon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.search),
+                            contentDescription = "home icon"
+                        )
+                    })
+                    BottomNavigationItem(selected = false, onClick = {
+
+                    }, icon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.person),
+                            contentDescription = "home icon"
+                        )
+                    })
+                }
+            }, content = {
+                Pages()
+            })
+        }
     }
 }
