a4ec18344a4bc1cc36ba6e161d07c02c214b8a37
ynsemreyildirim <yunusemreyildirim.inf.001@gmail.com>
2023-04-17 17:09:32 +0300

    changes in story view

--- app/src/main/java/com/yunusemreyildirim/instaclone/component/StoryAndProfilePhoto.kt
+++ app/src/main/java/com/yunusemreyildirim/instaclone/component/StoryAndProfilePhoto.kt
@@ -1,17 +1,46 @@
 package com.yunusemreyildirim.instaclone.component
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.material.Text
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 
 @Composable
 fun StoryAndProfilePhoto(
-    modifier: Modifier = Modifier
 ) {
-    Box(modifier) {
-        Text(text = "StoryAndProfilePhoto")
+    val borderBrush = Brush.linearGradient(colors = listOf(Color.Red, Color.Green, Color.Blue))
+    Row {
+        Spacer(modifier = Modifier.width(6.dp))
+        Box(
+            modifier = Modifier
+                .size(76.dp)
+                .clip(CircleShape)
+                .border(width = 2.dp, brush = borderBrush, shape = CircleShape)
+                .clickable(onClick = {
+                    //go profile
+                }),
+            contentAlignment = Alignment.Center
+        ) {
+            AsyncImage(
+                model = photo,
+                contentDescription = "post profile photo",
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .size(65.dp),
+                contentScale = ContentScale.Crop
+            )
+        }
+        Spacer(modifier = Modifier.width(6.dp))
     }
 }
 
--- app/src/main/java/com/yunusemreyildirim/instaclone/view/FeedPage.kt
+++ app/src/main/java/com/yunusemreyildirim/instaclone/view/FeedPage.kt
@@ -2,18 +2,23 @@ package com.yunusemreyildirim.instaclone.view
 
 import android.app.Activity
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import com.yunusemreyildirim.instaclone.component.Comment
 import com.yunusemreyildirim.instaclone.component.PostImage
+import com.yunusemreyildirim.instaclone.component.StoryAndProfilePhoto
 import com.yunusemreyildirim.instaclone.component.SubComment
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun FeedPage(context:Activity) {
+fun FeedPage(context: Activity) {
 
     val scope = rememberCoroutineScope()
     val bottomSheetState =
@@ -25,8 +30,7 @@ fun FeedPage(context:Activity) {
             BackHandler {
                 if (bottomSheetState.currentValue != ModalBottomSheetValue.Hidden) {
                     scope.launch { bottomSheetState.hide() }
-                }
-                else{
+                } else {
                     context.finish()
                 }
             }
@@ -41,7 +45,22 @@ fun FeedPage(context:Activity) {
             }
         },
     ) {
-        LazyColumn {
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            item {
+                LazyRow(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentHeight(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    item { StoryAndProfilePhoto() }
+                    item { StoryAndProfilePhoto() }
+                    item { StoryAndProfilePhoto() }
+                    item { StoryAndProfilePhoto() }
+                    item { StoryAndProfilePhoto() }
+                    item { StoryAndProfilePhoto() }
+                }
+            }
             item {
                 PostImage {
                     scope.launch { bottomSheetState.show() }
@@ -63,5 +82,6 @@ fun FeedPage(context:Activity) {
                 }
             }
         }
+
     }
 }
